4d65e73541ebc8da08e70eb6dc5870b08de74079
Kevin Ponto <github@kevinponto.com>
2014-02-26 16:36:01 -0800

    Better system feedback behavior

--- app/assets/javascripts/kevins_shitty_javascript.js
+++ app/assets/javascripts/kevins_shitty_javascript.js
@@ -1,10 +1,14 @@
 $(document).ready(function() { // bind to document.ready instead of window.load because of Turbolinks
 
-	// Fadeout element after delay - ie, system feedback notice
-     $(".js-auto_fade_away").delay("2000").fadeTo("slow", 0.01, function(){ //fade
-         $(this).slideUp("slow", function() { //slide up
-             $(this).remove(); //then remove from the DOM
-         });
-     });
+	$(".show_system_feedback").click(function() {
+		$("#system_feedback").removeClass("hide");
+	
+		// Fadeout element after delay - ie, system feedback notice
+		$(".js-auto_fade_away").delay("2000").fadeTo("slow", 0.01, function(){ //fade
+		    $(this).slideUp("slow", function() { //slide up
+		    	$(this).remove(); //then remove from the DOM
+		    });
+		});
+	});
 
 });
