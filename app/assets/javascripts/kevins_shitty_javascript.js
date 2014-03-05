510163c9233708f9d4528c55b3545ab68404d0ee
Kevin Ponto <github@kevinponto.com>
2014-03-04 21:39:22 -0800

    Expandable notifications and hover states

--- app/assets/javascripts/kevins_shitty_javascript.js
+++ app/assets/javascripts/kevins_shitty_javascript.js
@@ -1,14 +1,30 @@
 $(document).ready(function() { // bind to document.ready instead of window.load because of Turbolinks
 
-	$(".show_system_feedback").click(function() {
-		$("#system_feedback").removeClass("hide");
-	
-		// Fadeout element after delay - ie, system feedback notice
-		$(".js-auto_fade_away").delay("2000").fadeTo("slow", 0.01, function(){ //fade
-		    $(this).slideUp("slow", function() { //slide up
-		    	$(this).remove(); //then remove from the DOM
-		    });
-		});
-	});
 
+	// System feedback demo for settings page
+
+		$(".show_system_feedback").click(function() {
+			$("#system_feedback").removeClass("hide");
+		
+			// Fadeout element after delay - ie, system feedback notice
+			$(".js-auto_fade_away").delay("2000").fadeTo("slow", 0.01, function(){ //fade
+			    $(this).slideUp("slow", function() { //slide up
+			    	$(this).remove(); //then remove from the DOM
+			    });
+			});
+		});
+
+	// Notification expand demo for dashboard
+
+		$(".js_expand_notification").click(function() {
+			$(".js_expand_notification").animate({
+				height: "200px"
+			}, 250, function() {
+			    // Animation complete.
+			  });
+		});
+
+		$(".dismiss").click(function(){
+			$(this).parent().hide();
+		});
 });
