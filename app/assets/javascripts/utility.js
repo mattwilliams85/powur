5f13368d63c47485942b7e546104fae55dac7154
Rick Hou <Rick.Hou@EyeCueLab.com>
2014-05-16 11:36:19 -0700

    ajax uploader

--- app/assets/javascripts/utility.js
+++ app/assets/javascripts/utility.js
@@ -55,3 +55,73 @@ $.fn.serializeObject = function()
     });
     return o;
 };
+
+
+//Utlity to allow ajax upload
+//_formInputObj is the input in the form that is responsible for selecting files from desktop
+//_uploadEndpoint is the server-side endpoint that recieves the multipart upload
+//optoinal: _options._fileNameDisplay shows the file that's been selected by user
+//optional: _options._uploadProgressDisplay shows the upload progress
+//optional: _callback function that is triggered after the upload has been completed 
+function ajaxUpload(_formInputObj, _uploadEndpoint, _options, _callback){
+	var _formData = new FormData();
+	var _supportedFiles = /(\.jpg|\.jpeg|\.pdf|\.gif|\.png)$/i;
+
+	_formInputObj.on("change", function(e){
+		for(i=0;i<$(this).file.length;i++){
+			_file = $(this).file[i];
+
+			if(!!_file.type.match(_supportedFiles)){
+				//display filename is display object is provided
+				if(_options._uploadProgressDisplay !== undefined)
+					_options._uploadProgressDisplay.html("Uploading "+_file.filename);
+
+				_fileReader = new FileReader();
+				_fileReader.on("onloadend", function(e){
+					if(_options._uploadProgressDisplay !== undefined)
+						_options._uploadProgressDisplay.html(_file.filename+" upload completed");
+				});
+				_fileReader.readAsDataURL(_file);
+				_formData.append("attachments[]", _file);
+
+
+			}else{
+				if(_options._uploadProgressDisplay !== undefined)
+					_options._uploadProgressDisplay.html(_file.filename+" is not a supported file.");
+				console.log(_file.filename+" is not a supported file.")
+			}
+		}
+
+		//upload file
+		$.ajax({
+			url: _uploadEndpoint,
+			type: "POST",
+			data: _formdata,
+			processData: false,
+			contentType: false,
+			success: function (r) {
+				if(_callback!==undefined) _callback();
+			}
+		});
+	});
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
