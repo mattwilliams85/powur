3f4d6b810a9bd078ab29e8a11b75e401a42ffdd7
Kevin Ponto <github@kevinponto.com>
2014-05-16 15:17:50 -0700

    [heroku] undoing the json thing

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,3 @@
 Rails.application.config.assets.precompile += %w( Chart.js )
 
 Rails.application.config.assets.precompile += %w( index.css )
-
-Rails.application.config.assets.precompile += %w( users.100.json )
