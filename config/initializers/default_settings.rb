aad1a72c7931411eedef9cb8b52405f5c3e94440
Paul Walker <github@paulwalker.tv>
2014-05-30 13:58:27 -0700

    rescue all errros

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,5 +1,5 @@
 begin
   PromoterConfig.max_invites = 5
   PromoterConfig.invite_valid_days = 1
-rescue ActiveRecord::StatementInvalid, PG::UndefinableTable
+rescue Exception# ActiveRecord::StatementInvalid, PG::UndefinableTable
 end
