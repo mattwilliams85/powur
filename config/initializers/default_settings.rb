5e2680c9ccdcd0fb1c9c7f09d7cd447efad1e2c0
Paul Walker <github@paulwalker.tv>
2014-05-30 15:44:31 -0700

    check for settings table

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,5 +1,7 @@
 begin
-  PromoterConfig.max_invites = 5
-  PromoterConfig.invite_valid_days = 1
+  if ActiveRecord::Base.connection.tables.include?('settings')
+    PromoterConfig.max_invites = 5
+    PromoterConfig.invite_valid_days = 1
+  end
 rescue Exception# ActiveRecord::StatementInvalid, PG::UndefinableTable
 end
