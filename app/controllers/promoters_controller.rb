f4d6dbce7248ceb92eb36a708326233b3ff7ab84
Paul Walker <github@paulwalker.tv>
2014-06-01 14:46:14 -0700

    remove deprecated code

--- app/controllers/promoters_controller.rb
+++ app/controllers/promoters_controller.rb
@@ -7,15 +7,6 @@ class PromotersController < ApplicationController
   end
 
   def show
-    respond_to do |format|
-      format.json do
-        require_input :code
-
-        @invite = Invite.find(params[:code]) or error!(t('errors.invalid_code'), :code)
-
-        render 'users/registration'
-      end
-    end
   end
 
 end
