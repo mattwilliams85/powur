7d65f2ec76e37e5745a52cd774822390c98feebe
Paul Walker <github@paulwalker.tv>
2014-06-02 19:36:08 -0700

    seed data for everyone

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(
+User.create!(
   email:        'jon@sunstand.com', 
   password:     'solarpower', 
   first_name:   'Jonanthan', 
@@ -14,10 +14,26 @@ User.create(
   phone:        '858.555.1212',
   zip:          '92023' )
 
-User.create(
+User.create!(
   email:        'paul.walker@eyecuelab.com', 
   password:     'solarpower', 
   first_name:   'Paul', 
   last_name:    'Walker',
   phone:        '310.922.2629',
   zip:          '92127' )
+
+User.create!(
+  email:        'rick.hou@eyecuelab.com', 
+  password:     'solarpower', 
+  first_name:   'Rick', 
+  last_name:    'Hou',
+  phone:        '720.555.1212',
+  zip:          '97212' )
+
+User.create!(
+  email:        'kevin.ponto@eyecuelab.com', 
+  password:     'solarpower', 
+  first_name:   'Kevin', 
+  last_name:    'Ponto',
+  phone:        '805.555.1212',
+  zip:          '93101' )
