faeec058a7bf93684245e8f84bc0ba3ea7ee5487
Rick Hou <Rick.Hou@EyeCueLab.com>
2014-06-05 11:52:23 -0700

    wire up reset password flow

--- app/assets/javascripts/passwords.js
+++ app/assets/javascripts/passwords.js
@@ -1,1 +1,28 @@
 //passwords.js
+
+jQuery(function($){
+    //wire up reset form
+    $(document).on("click", "#user_reset_password_form button", function(e){
+        _formSubmit(e, $("#user_reset_password_form"), "/login/password/", "post", function(data, text){
+            if(typeof data.properties._message.confirm !=="undefined"){
+                $("#user_reset_password_form").append("<div class='js-message secondary_button' style='display:none; margin-top:100px; color:#20c2f1;'>"+data.properties._message.confirm+"</div>");
+                $("#user_reset_password_form .js-message").fadeIn();
+                $("#user_reset_password_form span.form_row, #user_reset_password_form button, #user_reset_password_form a").fadeOut();
+
+            }
+        });
+    });
+
+    //wire up update form
+    $(document).on("click", "#user_update_password_form button", function(e){
+        _formSubmit(e, $("#user_update_password_form"), "/login/password/", "put", function(data, text){
+            console.log(data);
+            if(typeof data.properties._message.confirm !=="undefined"){
+                $("#user_update_password_form").append("<div class='js-message secondary_button' style='display:none; margin-top:100px; color:#20c2f1;'>"+data.properties._message.confirm+"</div>");
+                $("#user_update_password_form .js-message").fadeIn();
+                $("#user_update_password_form span.form_row, #user_update_password_form button").fadeOut();
+            }
+        });
+    });
+
+});
