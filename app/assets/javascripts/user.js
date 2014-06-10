04428899c5151bd6d742002813f263f7a6cc8e52
Kevin Ponto <github@kevinponto.com>
2014-06-10 16:20:50 -0700

    [Form UI] Better form behavior - signup animations

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -9,3 +9,13 @@ jQuery(function($){
 	    });
     });
 });
+
+jQuery(function($){
+	$(document).ready(function(){
+		$(".js-multi_form_submit").on("click", function(e){
+               $(this).html('<i class="fa fa-check"></i> Saved!').addClass("saved_button");
+			$(this).parents(".js-multi_form_fieldset").addClass("saved_fieldset").next(".js-multi_form_fieldset").removeAttr("disabled");
+			e.preventDefault();
+		});
+    });
+});
