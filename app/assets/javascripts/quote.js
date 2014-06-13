9dc8899afcd4d001643dca0e7eb7f6e4687d62e9
Rick Hou <Rick.Hou@EyeCueLab.com>
2014-06-13 12:56:02 -0700

    wire up customer quote page

--- app/assets/javascripts/quote.js
+++ app/assets/javascripts/quote.js
@@ -3,45 +3,49 @@ var _data = {};
 jQuery(function($){
     $(document).ready(function(){
         $("#user_nav").css("display","none");
-
-        //******** remove below after testing **********//
         _data.quotes=[];
+        _data.customer={};
+
+        //receive existing details for current customers
         _ajax({_ajaxType:"get", _url:"/customers", _callback:function(data, text){
             //get latest quotes
             $.each(data.entities, function(key, val){
-                _data.quotes.push(val.properties);
+                //get detailed info for the customer
+                $.ajax({type:"get", url:"/customers/"+val.properties.id}).done(function(data,text){ _data.quotes.push(data.properties);});
             });
         }});
-        //******** remove above after testing **********//
 
+        //receive customer data from the uri
+        _ajax({_ajaxType:"get", _url:window.location.pathname, _callback:function(data,text){
+            _data.customer = data.properties;
+            $("#customer_signup").find(".js-multi_form_fieldset").removeAttr("disabled");
+
+            if(typeof _data.customer.email !== "undefined"){
+                //prepopulate form field values
+                $.each(_data.customer, function(key, val){
+                    $("#customer_signup").find("input[name='"+key+"']").val(val);
+                });
+            }else{
+
+                console.log("new user");
+            }
+        }});
     });
+
     $(document).on("click", "#customer_signup button", function(e){
-        //determine whether it's an existing quote based on the latest data
-        _data.quotes=[];
-        _ajax({_ajaxType:"get", _url:"/customers", _callback:function(data, text){
-            //get latest quotes
-            $.each(data.entities, function(key, val){
-                _data.quotes.push(val.properties);
-            });
-            //search to see if there is a matching email or phone 
-            _formData = $("#customer_signup").serializeObject();
-            _matchedQuote = _data.quotes.filter(function(quote){return quote.email==_formData.email;});
-            if(_matchedQuote.length==0) _matchedQuote=_data.quotes.filter(function(quote){return quote.phone==_formData.phone;});
+        //search to see if there is a matching email or phone 
+        _formData = $("#customer_signup").serializeObject();
+        _matchedQuote = _data.quotes.filter(function(quote){return quote.email==_formData.email;});
+        if(_matchedQuote.length==0) _matchedQuote=_data.quotes.filter(function(quote){return quote.phone==_formData.phone;});
 
-            //update or create new customers
-            _verb= (_matchedQuote.length>0)? "patch":"post";
-            _endPoint = (_matchedQuote.length>0)? "/customers":"/customers/"+_matchedQuote.id;
-            _ajax({_ajaxType:_verb, _url:_endPoint, _postObj:_formData, _callback:function(data, text){
-                console.log("data input complete");
-                //check to see if we are ready to open up additional fieldsets 
-
-
-            }});
-
+        //update or create new customers
+        _verb= (_matchedQuote.length>0)? "patch":"post";
+        _endPoint = (_matchedQuote.length>0)? "/customers/"+_matchedQuote[0].id:"/customers";
+        _ajax({_ajaxType:_verb, _url:_endPoint, _postObj:_formData, _callback:function(data, text){
+            console.log("data input complete");
+            //check to see if we are ready to open up additional fieldsets 
         }});
-
     });
-
 });
 
 
