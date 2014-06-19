74a42f661e832d2cfc2013dd9c7370dac51bdc88
Rick Hou <Rick.Hou@EyeCueLab.com>
2014-06-18 21:42:29 -0700

    invitation workflow tweaks

--- app/assets/javascripts/promoter.js
+++ app/assets/javascripts/promoter.js
@@ -4,15 +4,15 @@ jQuery(function($){
     $(document).ready(function(){
         _getRoot(function(){
             //populate form
-            _action = _data.root.actions.filter(function(action){return action.href=="/login/register";})[0];
-            for(i=0;i<_action.fields.length;i++)
-                $(".js-signup_form").find("input[name='"+_action.fields[i].name+"']").val(_action.fields[i].value)
+            _data.action = _data.root.actions.filter(function(action){return action.name=="create";})[0];
+            for(i=0;i<_data.action.fields.length;i++)
+                $(".js-signup_form").find("input[name='"+_data.action.fields[i].name+"']").val(_data.action.fields[i].value)
         });
     });
 
     $(document).on("click", ".js-signup_form button", function(e){
         e.preventDefault();
-        _formSubmit(e, $(".js-signup_form"), "/login/register", "post", function(data, text){
+        _formSubmit(e, $(".js-signup_form"), _data.action.href, _data.action.method, function(data, text){
             window.location = data.links.filter(function(link){return link.rel=="index"})[0].href;
         });
     });
