ca1bad4a22bcb6e263f22ea546a2688849a53d91
Paul Walker <github@paulwalker.tv>
2014-08-30 13:59:53 -0700

    reference user and product for order totals

--- app/controllers/admin/order_totals_controller.rb
+++ app/controllers/admin/order_totals_controller.rb
@@ -3,7 +3,8 @@ module Admin
   class OrderTotalsController < AdminController
 
     def index
-      @order_totals = sort_and_page(@order_totals)
+      @order_totals = sort_and_page(
+        @order_totals.includes(:product, :user).references(:product, :user))
     end
 
   end
