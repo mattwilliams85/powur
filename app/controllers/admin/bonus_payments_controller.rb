45a156f6307c4b19bf1c41399cc404659a83fe6f
Paul Walker <github@paulwalker.tv>
2014-10-01 16:32:31 -0700

    amount total aggregate for bonus payments

--- app/controllers/admin/bonus_payments_controller.rb
+++ app/controllers/admin/bonus_payments_controller.rb
@@ -5,6 +5,9 @@ module Admin
     def index
       @bonus_payments = apply_list_query_options(@bonus_payments).
         includes(:user, :bonus).references(:user, :bonus)
+      total_amount = @bonus_payments.except(:offset, :limit, :order).sum(:amount)
+      @totals = {
+        amount: total_amount }
     end
 
   end
