ffc7fccdcc1889c239fcb8009627433eda78553b
DannyAtEyeCueLab <daniel.mcalerney@eyecuelab.com>
2014-10-02 11:36:38 -0700

    refactor of listquery to rank_achievements

--- app/controllers/auth/rank_achievements_controller.rb
+++ app/controllers/auth/rank_achievements_controller.rb
@@ -1,10 +1,11 @@
 module Auth
 
   class RankAchievementsController < AuthController
+    include ListQuery
 
     def index
-      @rank_achievements = page!(
-        @rank_achievements.includes(:rank, :user).references(:rank, :user))
+      @rank_achievements = apply_list_query_options(@rank_achievements.
+        includes(:rank, :user).references(:rank, :user))
     end
 
   end
