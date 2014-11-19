58f3f588331e33db3e8167c09ae2127f94192777
mattwilliams85 <mattwilliams85@gmail.com>
2014-11-18 17:47:01 -0800

    user able to user forward/backward broswer buttons through wiselinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require wiselinks
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require wiselinks
 //= require velocity
 //= require handlebars
 //= require utility
