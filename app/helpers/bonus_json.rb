cbb10ec2e1c458f3a0827bb24858a4342f1ba620
mattwilliams85 <mattwilliams85@gmail.com>
2014-11-21 11:26:41 -0800

    temporarily fix issue with including BonusJson

--- app/helpers/bonus_json.rb
+++ app/helpers/bonus_json.rb
@@ -1,22 +1,38 @@
-class BonusJson < JsonDecorator
-  def list_init(partial_path = 'item')
-    klass :bonuses, :list
+# class BonusJson < JsonDecorator
+#   def list_init(partial_path = 'item')
+#     klass :bonuses, :list
 
-    list_entities(partial_path)
-  end
+#     list_entities(partial_path)
+#   end
 
-  module BonusActions
-    def amount_field(bonus)
-      field(:amounts, :dollar_percentage,
-            array:     true,
-            first:     bonus.rank_range.first,
-            last:      bonus.rank_range.last,
-            value:     bonus.normalized_amounts,
-            total:     bonus.available_amount,
-            remaining: bonus.remaining_amount,
-            max:       bonus.remaining_percentage)
-    end
+#   module BonusActions
+#     def amount_field(bonus)
+#       field(:amounts, :dollar_percentage,
+#             array:     true,
+#             first:     bonus.rank_range.first,
+#             last:      bonus.rank_range.last,
+#             value:     bonus.normalized_amounts,
+#             total:     bonus.available_amount,
+#             remaining: bonus.remaining_amount,
+#             max:       bonus.remaining_percentage)
+#     end
+#   end
+
+#   SirenJson::Action.include(self)
+# end
+
+module BonusJson
+  def amount_field(bonus)
+    field(:amounts, :dollar_percentage,
+          array:     true,
+          first:     bonus.rank_range.first,
+          last:      bonus.rank_range.last,
+          value:     bonus.normalized_amounts,
+          total:     bonus.available_amount,
+          remaining: bonus.remaining_amount,
+          max:       bonus.remaining_percentage)
   end
 
   SirenJson::Action.include(self)
 end
+
