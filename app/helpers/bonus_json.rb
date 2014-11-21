26ecbb41e0f6b23b2de66d054b36adff75264012
Paul Walker <github@paulwalker.tv>
2014-11-20 19:32:37 -0800

    ref fix

---
+++ app/helpers/bonus_json.rb
@@ -0,0 +1,22 @@
+class BonusJson < JsonDecorator
+  def list_init(partial_path = 'item')
+    klass :bonuses, :list
+
+    list_entities(partial_path)
+  end
+
+  module BonusActions
+    def amount_field(bonus)
+      field(:amounts, :dollar_percentage,
+            array:     true,
+            first:     bonus.rank_range.first,
+            last:      bonus.rank_range.last,
+            value:     bonus.normalized_amounts,
+            total:     bonus.available_amount,
+            remaining: bonus.remaining_amount,
+            max:       bonus.remaining_percentage)
+    end
+  end
+
+  SirenJson::Action.include(self)
+end
