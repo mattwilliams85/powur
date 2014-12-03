b596f7aa9a0060712e615949ccf296d8abe7b943
DannyAtEyeCueLab <daniel.mcalerney@eyecuelab.com>
2014-12-03 08:53:51 -0800

    stubbed out simple request spec.  More tests to come when we decide which NMI service will work in the project

--- spec/requests/auth/empower_merchant_request_spec.rb
+++ spec/requests/auth/empower_merchant_request_spec.rb
@@ -9,22 +9,9 @@ describe '/u/empower_merchant' do
   describe '#sandbox' do
     it "display has the text 'Empower Merchant Sandbox'" do
       get sandbox_empower_merchant_path
-      expect_200
+       expect_200
       expect(response.body).to match(/Empower Merchant Sandbox/)
     end
   end
 
-  it 'connects to the NMI empower merchant service' do
-      req
-      post bonus_requirements_path(@bonus), product_id: @product.id,
-        source: true, format: :json
-
-      requirements = json_body['entities'].find do |e|
-        e['class'].include?('requirements')
-      end
-      existing = requirements['entities'].find do |e|
-        e['properties']['product_id'] == req.product_id
-      end
-      expect(existing['properties']['source']).to eq(false)
-    end
 end
