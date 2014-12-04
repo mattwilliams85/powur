52c078ee25b102f5c891288b073bfe3c14abd7ac
DannyAtEyeCueLab <daniel.mcalerney@eyecuelab.com>
2014-12-04 15:53:52 -0800

    just changed some formatting

--- spec/seed/admin_seed_spec.rb
+++ spec/seed/admin_seed_spec.rb
@@ -3,9 +3,9 @@ require 'rake'
 
 describe 'admin seed' do
   before { Sunstand::Application.load_tasks }
-  it "should create more users" do
-    expect{ Rake::Task["db:seed"].invoke }.to change{User.count}.by(7)
+  it 'should create more users' do
+    expect { Rake::Task['db:seed'].invoke }.to change { User.count }.by(7)
   end
-  
+
   it { expect { Rake::Task['db:seed'].invoke }.not_to raise_exception }
 end
