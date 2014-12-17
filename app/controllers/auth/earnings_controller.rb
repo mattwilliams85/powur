98221d26316fe8fbeea52777e489eb43ec879760
mattwilliams85 <mattwilliams85@gmail.com>
2014-12-17 14:33:59 -0800

    major style update to hot quotes kpi; add temporary rank badges 1-3

--- app/controllers/auth/earnings_controller.rb
+++ app/controllers/auth/earnings_controller.rb
@@ -1,7 +1,25 @@
 module Auth
   class EarningsController < AuthController
     def show
-      render 'show.html.erb'
+      @user = current_user
+      @downline = @user.downline_users
+      @total_earnings = current_user.bonus_payments.sum(:amount) 
+      @downline.each do |user|
+        @total_earnings += user.bonus_payments.sum(:amount)
+      end
+      
+
+      # @total_amount = @bonus_payments
+      #  .except(:offset, :limit, :order).sum(:amount)
     end
   end
 end
+
+# def index
+#       total_amount = @bonus_payments
+#                      .except(:offset, :limit, :order).sum(:amount)
+#       @totals = [ { id: :bonus, value: total_amount, type: :currency } ]
+
+#       @bonus_payments = apply_list_query_options(@bonus_payments)
+#                         .includes(:user, :bonus).references(:user, :bonus)
+#     end
