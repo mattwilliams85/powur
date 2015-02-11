e9a3d9e19c61e0929ba6abdf8739c98c9622a7ea
mattwilliams85 <mattwilliams85@gmail.com>
2015-02-10 17:32:56 -0800

    turned off alternate tab color

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1515,12 +1515,12 @@ function Dashboard(){
   function _alternateColor(_drillDownLevel){
     if(_drillDownLevel % 2 === 0){
        $('#dashboard_team').find('.drilldown.level_'+_drillDownLevel).find('.drilldown_content').css('background-color',"#333") 
-       $('.drilldown.level_'+(_drillDownLevel - 1)).find('.active_tab').css('background-color',"#333") 
-       $('.drilldown.level_'+_drillDownLevel).find('.active_tab').css('background-color',"#444") 
+       // $('.drilldown.level_'+(_drillDownLevel - 1)).find('.active_tab').css('background-color',"#333") 
+       // $('.drilldown.level_'+_drillDownLevel).find('.active_tab').css('background-color',"#444") 
     } else {
        $('#dashboard_team').find('.drilldown.level_'+_drillDownLevel).find('.drilldown_content').css('background-color',"#444")
-       $('.drilldown.level_'+_drillDownLevel).find('.active_tab').css('background-color',"#333")
-       $('.drilldown.level_'+(_drillDownLevel - 1)).find('.active_tab').css('background-color',"#444")   
+       // $('.drilldown.level_'+_drillDownLevel).find('.active_tab').css('background-color',"#333")
+       // $('.drilldown.level_'+(_drillDownLevel - 1)).find('.active_tab').css('background-color',"#444")   
     }
   }
 
