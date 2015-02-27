37d37072bcf34266b3441a2a83160c138a416fde
Andrew M Westling <a@andrewwestling.com>
2015-02-26 17:15:26 -0800

    begin library drilldowns (WIP)

--- app/assets/javascripts/angular/library/controller.js
+++ app/assets/javascripts/angular/library/controller.js
@@ -9,10 +9,23 @@ function LibraryCtrl($scope, $location) {
   
   $scope.libraryList = items;
 
-  $scope.doSomething = function(item) {
-
+  $scope.setClass = function(id, prefix) {
+    return prefix + '-' + id; //e.g. 'drilldown-23'
   };
-};
+
+  $scope._drillDown = function(id) {
+    var drillDownHeight = '220px';
+    if ($('.thumbnail-' + id).css('margin-bottom') === drillDownHeight) {
+      $('.thumbnail-' + id).animate({'margin-bottom': '0px'}, 500);
+      $('.drilldown-' + id).animate({'opacity': 0}, 500);
+      $('.drilldown-' + id).animate({'display': 'none'}, 500);
+    } else {
+      $('.thumbnail-' + id).animate({'margin-bottom': drillDownHeight}, 500);
+      $('.drilldown-' + id).animate({'opacity': 1}, 500);
+      $('.drilldown-' + id).css({'display': 'block'});
+    }
+  };
+}
 
 LibraryCtrl.$inject = ['$scope', '$location'];
 sunstandControllers.controller('LibraryCtrl', LibraryCtrl);
