cda0a6d119437b64f79999ece3c2b7a4ad1765f5
Andrew M Westling <a@andrewwestling.com>
2015-02-27 11:03:26 -0800

    library front-end (WIP)

--- app/assets/javascripts/angular/library/controller.js
+++ app/assets/javascripts/angular/library/controller.js
@@ -13,17 +13,20 @@ function LibraryCtrl($scope, $location) {
     return prefix + '-' + id; //e.g. 'drilldown-23'
   };
 
-  $scope._drillDown = function(id) {
-    var drillDownHeight = '220px';
-    if ($('.thumbnail-' + id).css('margin-bottom') === drillDownHeight) {
-      $('.thumbnail-' + id).animate({'margin-bottom': '0px'}, 500);
-      $('.drilldown-' + id).animate({'opacity': 0}, 500);
-      $('.drilldown-' + id).animate({'display': 'none'}, 500);
-    } else {
-      $('.thumbnail-' + id).animate({'margin-bottom': drillDownHeight}, 500);
-      $('.drilldown-' + id).animate({'opacity': 1}, 500);
-      $('.drilldown-' + id).css({'display': 'block'});
-    }
+  $scope.drillDown = function(item) {
+    // var drillDownHeight = '220px';
+    // if ($('.thumbnail-' + id).css('margin-bottom') === drillDownHeight) {
+    //   $('.thumbnail-' + id).animate({'margin-bottom': '0px'}, 500);
+    //   $('.drilldown-' + id).animate({'opacity': 0}, 500);
+    //   $('.drilldown-' + id).animate({'display': 'none'}, 500);
+    // } else {
+    //   $('.thumbnail-' + id).animate({'margin-bottom': drillDownHeight}, 500);
+    //   $('.drilldown-' + id).animate({'opacity': 1}, 500);
+    //   $('.drilldown-' + id).css({'display': 'block'});
+    // }
+    var html = '';
+    $('<div class=\'reveal-modal library-modal\' data-reveal>' + html + '<a class=\'close-reveal-modal\'>&#215;</a></div>').foundation('reveal', 'open');
+    $(document).foundation();
   };
 }
 
