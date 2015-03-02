785a8daefadb5d7bda81ac8c1904fb9dc0976516
Andrew M Westling <a@andrewwestling.com>
2015-03-02 13:11:40 -0800

    create service for library items; update template/css

--- app/assets/javascripts/angular/library/controller.js
+++ app/assets/javascripts/angular/library/controller.js
@@ -1,34 +1,24 @@
 'use strict';
 
-function LibraryCtrl($scope, $location) {
+function LibraryCtrl($scope, $location, Resource) {
   $scope.redirectUnlessSignedIn();
-  
-  // return Library.list().then(function(items) {
-  // });
-  var items = ["a video", "a video", "a video", "a document", "a document", "a document"];
-  
-  $scope.libraryList = items;
+  $scope.items = [];
 
   $scope.setClass = function(id, prefix) {
     return prefix + '-' + id; //e.g. 'drilldown-23'
   };
 
-  $scope.drillDown = function(item) {
-    // var drillDownHeight = '220px';
-    // if ($('.thumbnail-' + id).css('margin-bottom') === drillDownHeight) {
-    //   $('.thumbnail-' + id).animate({'margin-bottom': '0px'}, 500);
-    //   $('.drilldown-' + id).animate({'opacity': 0}, 500);
-    //   $('.drilldown-' + id).animate({'display': 'none'}, 500);
-    // } else {
-    //   $('.thumbnail-' + id).animate({'margin-bottom': drillDownHeight}, 500);
-    //   $('.drilldown-' + id).animate({'opacity': 1}, 500);
-    //   $('.drilldown-' + id).css({'display': 'block'});
-    // }
-    var html = '';
+  $scope.showResource = function(item) {
+    var html = $('#item_' + item.properties.id).html();
     $('<div class=\'reveal-modal library-modal\' data-reveal>' + html + '<a class=\'close-reveal-modal\'>&#215;</a></div>').foundation('reveal', 'open');
     $(document).foundation();
   };
+
+  return Resource.list().then(function(items) {
+    $scope.items = items.entities;
+  });
+
 }
 
-LibraryCtrl.$inject = ['$scope', '$location'];
+LibraryCtrl.$inject = ['$scope', '$location', 'Resource'];
 sunstandControllers.controller('LibraryCtrl', LibraryCtrl);
