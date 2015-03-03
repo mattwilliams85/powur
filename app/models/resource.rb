413c7207dec2836f7310b0b667fffe2c3502e647
Andrew M Westling <a@andrewwestling.com>
2015-03-03 15:37:34 -0800

    add validation for Library resource title length

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -8,7 +8,7 @@ class Resource < ActiveRecord::Base
   belongs_to :user
 
   validates :user_id, presence: true
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 100 }
   validates :description, presence: true
   validates :file_original_path, presence: true
   validates :file_type,
