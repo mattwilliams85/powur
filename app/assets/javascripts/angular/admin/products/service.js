8b4cdbf742a6a920219c62cd977adecb6b69e487
Andrew M Westling <a@andrewwestling.com>
2015-03-24 17:35:16 -0700

    wire up Products list

---
+++ app/assets/javascripts/angular/admin/products/service.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function AdminProduct($http, $q) {
+  var service = {
+
+    /*
+    * Get list of Products
+    */
+    list: function() {
+      var dfr = $q.defer();
+
+      $http({
+        method: 'GET',
+        url: '/a/products.json'
+      }).success(function(res) {
+        dfr.resolve(res);
+      }).error(function(err) {
+        console.log('エラー', err);
+        dfr.reject(err);
+      });
+
+      return dfr.promise;
+    }
+  };
+
+  return service;
+}
+
+AdminProduct.$inject = ['$http', '$q'];
+sunstandServices.factory('AdminProduct', AdminProduct);
