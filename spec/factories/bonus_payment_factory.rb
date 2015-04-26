ab6b1167068c89dffe80048c70cec42f3f9e8d31
Paul Walker <paul@Pauls-MacBook-Pro.local>
2015-04-25 18:18:31 -0700

    use block when using settings for factory

--- spec/factories/bonus_payment_factory.rb
+++ spec/factories/bonus_payment_factory.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :bonus_payment do
     pay_period { create(:monthly_pay_period, at: DateTime.current - 1.month) }
     user
-    pay_as_rank SystemSettings.min_rank
+    pay_as_rank { SystemSettings.min_rank }
     association :bonus, factory: :seller_bonus
     amount { Faker::Commerce.price }
   end
