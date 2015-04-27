400b4fd92436323011d2a915f7ddc8652eac6223
Sasha Shamne <alexander.shamne@gmail.com>
2015-04-27 16:51:07 -0700

    Remove old js/css manifests

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,12 +1,10 @@
 # Javascripts
 Rails.application.config.assets.precompile += [
-  'new/application.js',
   'new/admin.js',
   'modernizr/modernizr.js'
 ]
 
 # Stylesheets
 Rails.application.config.assets.precompile += [
-  'new/application.css',
   'new/admin.css'
 ]
