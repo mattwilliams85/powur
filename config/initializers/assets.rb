e47a32e0095469d8427fc23a0bbdb6351ce8fda5
Sasha Shamne <alexander.shamne@gmail.com>
2015-04-27 15:39:46 -0700

    Cleanup assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,43 +1,10 @@
 # Javascripts
 Rails.application.config.assets.precompile += [
-  'admin/users.js',
-  'admin/products.js',
-  'admin/quotes.js',
-  'admin/notifications.js'
-]
-Rails.application.config.assets.precompile += %w( earnings.js )
-Rails.application.config.assets.precompile += %w( creditly.js )
-Rails.application.config.assets.precompile += %w( dashboard.js )
-Rails.application.config.assets.precompile += %w( dashboard-topper.js )
-Rails.application.config.assets.precompile += %w( kpi.js )
-Rails.application.config.assets.precompile += %w( profile.js )
-Rails.application.config.assets.precompile += %w( promoter.js )
-Rails.application.config.assets.precompile += %w( quote.js )
-Rails.application.config.assets.precompile += %w( user.js )
-Rails.application.config.assets.precompile += %w( jquery.pagepiling.js )
-
-Rails.application.config.assets.precompile += [
   'new/application.js',
   'new/admin.js'
 ]
 
-# Vendor Javascripts
-Rails.application.config.assets.precompile += %w( Chart.js )
-
 # Stylesheets
-Rails.application.config.assets.precompile += [
-  'admin/users.css',
-  'admin/notifications.css'
-]
-Rails.application.config.assets.precompile += %w( creditly.css )
-Rails.application.config.assets.precompile += %w( index.css )
-Rails.application.config.assets.precompile += %w( profile.css )
-Rails.application.config.assets.precompile += %w( promoter.css )
-Rails.application.config.assets.precompile += %w( user.css )
-Rails.application.config.assets.precompile += %w( earnings.css )
-Rails.application.config.assets.precompile += %w( kpi.css )
-Rails.application.config.assets.precompile += %w( jquery.pagepiling.css )
-
 Rails.application.config.assets.precompile += [
   'new/application.css',
   'new/admin.css'
