146e028f7c2bf341e89cf63d8334391994ee0761
Sasha Shamne <alexander.shamne@gmail.com>
2015-05-26 15:52:52 -0700

    Fix Ipayout requests (partial fix, other part is on ipayout repo)

--- config/initializers/eyecue_ipayout.rb
+++ config/initializers/eyecue_ipayout.rb
@@ -1,5 +1,6 @@
 Ipayout.configure do |config|
   config.endpoint = ENV['IPAYOUT_API_ENDPOINT']
+  config.proxy = ENV['QUOTAGUARDSTATIC_URL']
   config.merchant_guid = ENV['IPAYOUT_MERCHANT_GUID']
   config.merchant_password = ENV['IPAYOUT_MERCHANT_PASSWORD']
 end
