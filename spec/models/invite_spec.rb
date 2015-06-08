d5b3270757d8dd921d3e1c4257e8d8bc3b1c3665
Sasha Shamne <alexander.shamne@gmail.com>
2015-06-08 10:55:16 -0700

    Update tos validation. Fixes #56

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Invite do
-
   it 'autogenerates an id' do
     invite = create(:invite)
     expect(invite.id).to_not be_nil
@@ -26,5 +25,41 @@ describe Invite do
   it 'does not allow an empty string on phone' do
     expect { create(:invite, phone: '') }
       .to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  describe '#accept' do
+    let(:user) { invite.accept(params) }
+    let(:invite) { create(:invite) }
+    let(:params) do
+      {
+        first_name: 'Donald',
+        last_name: 'Duck',
+        password: 'password',
+        password_confirmation: 'password'
+      }
+    end
+    let(:agreement) { double(version: 'mahalo') }
+
+    context 'active tos exist' do
+      before do
+        allow(ApplicationAgreement).to receive(:current).and_return(agreement)
+      end
+
+      it 'should validate latest tos' do
+        expect(user.errors.messages).to eq(
+          tos: ['Please read and agree to the latest terms and conditions in the Application and Agreement']
+        )
+      end
+    end
+
+    context 'no currently active tos' do
+      before do
+        allow(ApplicationAgreement).to receive(:current).and_return(nil)
+      end
+
+      it 'should let create a user' do
+        expect(user.errors.messages).to be_empty
+      end
+    end
   end
 end
