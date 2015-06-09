86e8230ff863d14a1e00c3112c60385e2e7e7df9
Andrew M Westling <a@andrewwestling.com>
2015-06-09 14:19:07 -0700

    fix punctuation on Social Media share quote

--- app/assets/javascripts/dashboard/controller.js
+++ app/assets/javascripts/dashboard/controller.js
@@ -161,7 +161,7 @@
 
     $scope.showBetaDashboardVideoLink = true;
 
-    $scope.socialQuote = 'Don\'t try to fight the existing reality, build a new model that makes the old model obsolete\' - Buckminster Fuller';
+    $scope.socialQuote = '"Don\'t try to fight the existing reality, build a new model that makes the old model obsolete." - Buckminster Fuller';
   }
 
   DashboardCtrl.$inject = ['$scope', '$rootScope', '$location', '$timeout', 'UserProfile', 'CommonService'];
