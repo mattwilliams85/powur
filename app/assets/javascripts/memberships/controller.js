e56853c392afff56abb85d9a64078f5695eb469f
Sasha Shamne <alexander.shamne@gmail.com>
2015-06-19 16:26:43 -0700

    Add memberships page. Fixes #89

--- app/assets/javascripts/memberships/controller.js
+++ app/assets/javascripts/memberships/controller.js
@@ -1,14 +1,26 @@
 ;(function() {
   'use strict';
 
-  function MembershipsCtrl($scope, UserProfile) {
+  function MembershipsCtrl($scope, $location, $anchorScroll, UserProfile) {
     $scope.redirectUnlessSignedIn();
 
     UserProfile.get().then(function(user) {
       $scope.currentUser = user;
     });
+
+    $scope.selectMembership = function(item) {
+      $scope.showModal('Purchase modal. Coming soon ...');
+      $anchorScroll();
+    };
+
+    $scope.scrollTo = function(domId) {
+      var old = $location.hash();
+      $location.hash(domId);
+      $anchorScroll();
+      $location.hash(old);
+    };
   }
 
-  MembershipsCtrl.$inject = ['$scope', 'UserProfile'];
+  MembershipsCtrl.$inject = ['$scope', '$location', '$anchorScroll', 'UserProfile'];
   angular.module('powurApp').controller('MembershipsCtrl', MembershipsCtrl);
 })();
