f861496635539bf59eb9f231d71f552733cd2127
Sasha Shamne <alexander.shamne@gmail.com>
2015-06-19 11:01:15 -0700

    Remove ipayout account creation from 'after class complete' hook

--- app/models/product_enrollment.rb
+++ app/models/product_enrollment.rb
@@ -5,12 +5,12 @@ class ProductEnrollment < ActiveRecord::Base
   belongs_to :user
 
   validates :product_id,
-    presence:   true,
-    uniqueness: { scope: :user_id }
+            presence:   true,
+            uniqueness: { scope: :user_id }
   validates :user_id, presence: true
 
   scope :incomplete, -> { where("state != 'completed'") }
-  scope :user_product, ->(user_id, product_id) {
+  scope :user_product, -> (user_id, product_id) {
     where(user_id: user_id, product_id: product_id)
   }
 
@@ -28,8 +28,9 @@ class ProductEnrollment < ActiveRecord::Base
     event :complete do
       transitions from: [:enrolled, :started], to: :completed
       after do
+        # TODO: ipayout account creation should be moved elsewhere
         # Create IPayout account if user passed required class (FIT)
-        find_or_create_ipayout_account(user) if product.is_required_class
+        # find_or_create_ipayout_account(user) if product.is_required_class
         UserUserGroup.populate_for_user_product(user_id, product_id)
         user.rank_up! if user.needs_rank_up?
       end
