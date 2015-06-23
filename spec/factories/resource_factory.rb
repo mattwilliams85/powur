e619b70e804a48a7539122da245c222f5e5d8c0a
Sasha Shamne <alexander.shamne@gmail.com>
2015-06-22 17:45:22 -0700

    Fix failing specs

--- spec/factories/resource_factory.rb
+++ spec/factories/resource_factory.rb
@@ -5,5 +5,10 @@ FactoryGirl.define do
     description 'Description'
     file_original_path 'http://eyecuelab.com/doc.pdf'
     is_public true
+    topic_id 1
+  end
+
+  factory :resource_topic do
+    title 'Title'
   end
 end
