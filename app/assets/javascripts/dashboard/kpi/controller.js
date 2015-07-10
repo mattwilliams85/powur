11c7f9ede6b074548b8faec70f73859abf3daa62
mattwilliams85 <mattwilliams85@gmail.com>
2015-07-10 13:22:41 -0700

    fixes genealogy-kpi data clumping

--- app/assets/javascripts/dashboard/kpi/controller.js
+++ app/assets/javascripts/dashboard/kpi/controller.js
@@ -179,11 +179,18 @@
       var newData = [];
       var data = $scope.settings[0].datasets[j].data;
       for(var i = 0; i < data.length; i+=7) {
-        newData[i/7] = 0;
-        if($scope.section === "genealogy") newData[i/7] = data[i];
+        if($scope.section === "genealogy") {
+          newData[i/7] = data[i - 1] || 0;
 
-        for(var n = 0; n < 7; n++) {
-          newData[i/7] += (data[i + n] || 0)
+          for(var n = 0; n < 7; n++) {
+            if(data[i + n] > newData[i/7]) newData[i/7] = data[i + n]
+          }
+        } else {
+          newData[i/7] = 0;
+
+          for(var n = 0; n < 7; n++) {
+            newData[i/7] += (data[i + n] || 0)
+          }
         }
       }
      $scope.settings[0].datasets[j].data = newData;
