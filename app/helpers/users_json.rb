10b408875e48c4f4470ba57509a922cd74a5062a
Andrew M Westling <a@andrewwestling.com>
2015-07-15 14:41:09 -0700

    re: #266: add Last Sign In date/time to overview tab

--- app/helpers/users_json.rb
+++ app/helpers/users_json.rb
@@ -41,7 +41,7 @@ class UsersJson < JsonDecorator
   def detail_properties(user = @item) # rubocop:disable Metrics/AbcSize
     list_item_properties
     json.properties do
-      json.call(user, :address, :city, :state, :zip, :profile, :avatar, :avatar_file_name)
+      json.call(user, :address, :city, :state, :zip, :profile, :avatar, :avatar_file_name, :last_sign_in_at)
       json.organic_rank rank_title(user.organic_rank)
       json.lifetime_rank rank_title(user.lifetime_rank)
       json.downline_count user.downline_users_count(user.id)
