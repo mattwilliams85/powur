b168601fa11a60f240db320218b4a2e3af5556b1
Andrew M Westling <a@andrewwestling.com>
2015-07-22 13:30:24 -0700

    close #304: invite email links now point to /sign-up/C0DE42

--- app/mailers/promoter_mailer.rb
+++ app/mailers/promoter_mailer.rb
@@ -2,7 +2,7 @@ class PromoterMailer < ActionMailer::Base
   def invitation(invite)
     to = "#{invite.full_name} <#{invite.email}>"
     # url = root_url(code: invite.id)
-    url = root_url + "create-wealth/" + invite.id
+    url = root_url + "sign-up/" + invite.id
     merge_vars = { code: invite.id, invite_url: url, sponsor: User.find(invite.sponsor_id).full_name }
 
     mail_chimp to, :invite, merge_vars
