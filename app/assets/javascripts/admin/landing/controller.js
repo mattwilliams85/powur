b5fdd5fa2932d8b52134a0553436f723a95bf45e
Sasha Shamne <alexander.shamne@gmail.com>
2015-07-23 11:51:11 -0700

    Update admin landing. References #305

--- app/assets/javascripts/admin/landing/controller.js
+++ app/assets/javascripts/admin/landing/controller.js
@@ -5,13 +5,28 @@
     $scope.redirectUnlessSignedIn();
 
     $scope.index = {};
+    $scope.daysAgoOptions = [
+      { label: 'Since Midnight', value: 1 },
+      { label: 'Last 7 days', value: 7 },
+      { label: 'Last 10 days', value: 10 },
+      { label: 'Last 30 days', value: 30 }
+    ];
 
-    $http({
-      method: 'GET',
-      url: '/a'
-    }).success(function success(data) {
-      $scope.index.data = data;
-    });
+    $scope.daysAgo = 1;
+
+    $scope.getData = function() {
+      $http({
+        method: 'GET',
+        url: '/a',
+        params: {
+          days_ago: $scope.daysAgo
+        }
+      }).success(function success(data) {
+        $scope.index.data = data;
+      });
+    };
+
+    $scope.getData();
   }
 
   LandingCtrl.$inject = ['$scope', '$http'];
