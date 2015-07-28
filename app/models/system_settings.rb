f6ba8fa49a8cee9e6a8f098134544c9daa5e6c49
Sasha Shamne <alexander.shamne@gmail.com>
2015-07-28 16:58:18 -0700

    Add optional system settings value request avoiding cache

--- app/models/system_settings.rb
+++ app/models/system_settings.rb
@@ -3,5 +3,14 @@ class SystemSettings < RailsSettings::CachedSettings
     def default_product
       Product.find(default_product_id)
     end
+
+    # Returns value
+    # forcing database call and cache rewrite
+    def get!(key)
+      record = object(key)
+      return unless record
+      record.rewrite_cache
+      record.value
+    end
   end
 end
