0c50c6b2b978773bb9cea658414179efb8337159
Sasha Shamne <alexander.shamne@gmail.com>
2015-07-31 11:43:37 -0700

    Add profile page 'create ewallet account' option. Closes #342

--- app/models/product_enrollment.rb
+++ app/models/product_enrollment.rb
@@ -33,9 +33,6 @@ class ProductEnrollment < ActiveRecord::Base
     event :complete do
       transitions from: [:enrolled, :started], to: :completed
       after do
-        # TODO: ipayout account creation should be moved elsewhere
-        # Create IPayout account if user passed required class (FIT)
-        # find_or_create_ipayout_account(user) if product.is_required_class
         UserUserGroup.populate_for_user_product(user_id, product_id)
         user.rank_up! if user.needs_rank_up?
       end
