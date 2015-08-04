d50ea601b92d0e3c531b4d1131a0ef82403e9509
mattwilliams85 <mattwilliams85@gmail.com>
2015-08-04 15:18:45 -0700

    small refactor for kpi front-end

--- app/controllers/auth/kpi_metrics_controller.rb
+++ app/controllers/auth/kpi_metrics_controller.rb
@@ -18,15 +18,6 @@ module Auth
       render 'auth/kpi_metrics/proposals/show'
     end
 
-    def proposals_index
-      @user = User.find(params[:id])
-      @users = apply_list_query_options(User.with_ancestor(@user.id).lead_count)
-
-      @max_page = (pager.meta[:item_count] / 4.to_f).ceil
-
-      render 'auth/kpi_metrics/proposals/index'
-    end
-
     def genealogy_show
       @user = User.find(params[:id])
       scale = params[:scale].to_i + 1
@@ -34,14 +25,6 @@ module Auth
         .where('created_at >= ?', scale.days.ago)
 
       render 'auth/kpi_metrics/genealogy/show'
-    end
-
-    def genealogy_index
-      @user = User.find(params[:id])
-      @users = apply_list_query_options(User.growth_performance(@user.id))
-      @max_page = (pager.meta[:item_count] / 4).to_f.ceil
-
-      render 'auth/kpi_metrics/genealogy/index'
     end
   end
 end
