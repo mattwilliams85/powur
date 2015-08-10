95ed8c275400880997919b8f7ef67cd6a3388cca
Paul Walker <github@paulwalker.tv>
2015-08-10 13:49:59 -0700

    filter list of pay periods by user if not admin

--- app/controllers/auth/pay_periods_controller.rb
+++ app/controllers/auth/pay_periods_controller.rb
@@ -7,12 +7,19 @@ module Auth
          end_date_desc:   { end_date: :desc },
          end_date_asc:    { end_date: :asc }
 
-    before_action :generate_missing, only: [ :index ]
+    before_action :fetch_user!, :generate_missing, only: [ :index ]
 
     filter :time_span, options: [ :monthly, :weekly ]
 
     def index
-      @pay_periods = apply_list_query_options(PayPeriod)
+      query = PayPeriod.all
+      if @user
+        from = [
+          user.created_at.beginning_of_week,
+          user.created_at.beginning_of_month ].min
+        query = query.where('start_date >= ?', from)
+      end
+      @pay_periods = apply_list_query_options(query)
     end
 
     def show
