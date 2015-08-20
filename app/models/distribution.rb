4b9d3736c4f39e26352d1ac73ec262ef93835e1b
Sasha Shamne <alexander.shamne@gmail.com>
2015-08-20 16:51:43 -0700

    Update payments of breakage accounts properly. References #443

--- app/models/distribution.rb
+++ app/models/distribution.rb
@@ -7,6 +7,10 @@ class Distribution < ActiveRecord::Base
 
   after_create :set_batch_id
 
+  def ewallet_client
+    @ewallet_client ||= EwalletClient.new
+  end
+
   def distribute_pay_period!(pay_period)
     payments = pay_period.bonus_payments.pending.with_ewallets.entries
     payment_ids = payments.map(&:id)
@@ -18,19 +22,11 @@ class Distribution < ActiveRecord::Base
   end
 
   def distribute!(payments)
-    client = EwalletClient.new
-
     batches = prepare_for_distribution(payments)
     successful_payment_ids = []
     batches.each do |user_id, data|
-      begin
-        client.ewallet_individual_load(
-          batch_id: batch_id,
-          payment:  data[:distribution_data])
+      if process_payment(user_id, data[:distribution_data])
         successful_payment_ids.concat(data[:payment_ids])
-      rescue Ipayout::Error::EwalletNotFound => e
-        User.find_by_id(user_id).update_attribute(:ewallet_username, nil)
-        Airbrake.notify(e)
       end
     end
     BonusPayment.where(id: successful_payment_ids).update_all(
@@ -53,6 +49,10 @@ class Distribution < ActiveRecord::Base
   def prepare_for_distribution(payments)
     batches = {}
     payments.each do |payment|
+      if payment.user.breakage_account?
+        payment.breakage!
+        next
+      end
       batch = batches[payment.user_id] || {
         payment_ids:       [],
         distribution_data: {
@@ -66,6 +66,16 @@ class Distribution < ActiveRecord::Base
     end
 
     batches
+  end
+
+  def process_payment(user_id, distribution_data)
+    ewallet_client.ewallet_individual_load(
+      batch_id: batch_id,
+      payment:  distribution_data)
+  rescue Ipayout::Error::EwalletNotFound => e
+    User.find_by_id(user_id).update_attribute(:ewallet_username, nil)
+    Airbrake.notify(e)
+    return nil
   end
 
   def cancel
