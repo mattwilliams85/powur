9d518304cd37c1acc62d34a5d101abb3d5fac9b5
Viphak Lay <viphak.lay@eyecuelab.com>
2015-10-02 11:09:37 -0700

    clean up

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,6 @@ Rails.application.config.assets.precompile += [
   'admin.css'
 ]
 
-Rails.application.config.assets.precompile += %w( next.css )
+Rails.application.config.assets.precompile += %w( client.css )
 Rails.application.config.assets.precompile += %w( client.js )
 
