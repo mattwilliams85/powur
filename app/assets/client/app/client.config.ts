2d83f79cf6cb73ee2f2ab5fb9baf5ec04eaef3d5
Viphak Lay <viphak.lay@eyecuelab.com>
2015-10-08 08:44:01 -0700

    another fix

--- app/assets/client/app/client.config.ts
+++ app/assets/client/app/client.config.ts
@@ -1,9 +1,4 @@
 /// <reference path='../typings/tsd.d.ts' />
-declare var valueModule: ng.IModule;
-declare var serviceModule: ng.IModule;
-declare var directiveModule: ng.IModule;
-declare var filtersModule: ng.IModule;
-declare var controllerModule: ng.IModule;
 declare var appModule: ng.IModule;
     
 module powur {
