46a1f8b87ad3d5ab6e2c12201834c0de3b9b4ae0
Viphak Lay <viphak.lay@eyecuelab.com>
2015-10-13 08:00:25 -0700

    auth

--- app/assets/client/app/services/auth-interceptor.service.ts
+++ app/assets/client/app/services/auth-interceptor.service.ts
@@ -3,35 +3,37 @@
 module powur {
   class AuthInterceptor {
     static ServiceId = 'AuthInterceptor'; 
-    static $inject = ['$log', '$q', '$location', '$injector'];
+    static $inject = ['$log', '$q'];
     
     constructor(private $log: ng.ILogService, 
-                private $q: ng.IQService,
-                private $location: ng.ILocationService,
-                private $injector: ng.auto.IInjectorService) {
+                private $q: ng.IQService) {
       
       $log.debug('setting up interceptor');
       return <any>{
-        // 'request': (config: any) => {
-        //   this.$log.debug(AuthInterceptor.ServiceId + ':request', config);
+        request: (config: any) => {
+          this.$log.debug(AuthInterceptor.ServiceId + ':request', config);
 
-        //   return config || $q.when(config);
-        // },
+          return config || $q.when(config);
+        },
         // 'response': (response: any) => {
         //   this.$log.debug(AuthInterceptor.ServiceId + ':response');
         //   return response || $q.when(response);
         // },
 
-        'reponseError': (response: any) => {
-          this.$log.debug(AuthInterceptor.ServiceId + ':reponseError');
+        responseError: (response: any) => {
+          this.$log.debug(AuthInterceptor.ServiceId + ':responseError');
   
-          // if (response.status === 401) {
-          //   this.$injector.invoke(($session: ISessionService) => {
-          //     $session.logout().then((r: ng.IHttpPromiseCallbackArg<any>) => {
-          //       this.$location.path('/');
-          //     })
-          //   })
-          // }
+          if (response.status === 401) {
+            //this.cache.clearAllSession();
+            RootController.get().$session.logout().then((r: ng.IHttpPromiseCallbackArg<any>) => {
+              RootController.get().$state.go('login.public');
+            })            
+            //this.$injector.invoke(($session: ISessionService) => {
+              //$session.logout().then((r: ng.IHttpPromiseCallbackArg<any>) => {
+                //this.$location.path('/');
+              //})
+            //})
+          }
           
           return $q.reject(response);
         }
--- app/assets/client/app/services/cache.service.ts
+++ app/assets/client/app/services/cache.service.ts
@@ -1,34 +1,34 @@
 /// <reference path='../_references.ts' />
 
 module powur {
-  export interface IUser {
-    displayName: string;
-  }
-  
-  export interface ICacheService {
-    user: IUser;
-    clearAllSession(): void;
-    clearAllStorage(): void;
-  }
+  // export interface IUser {
+  //   displayName: string;
+  // }
+  // 
+   export interface ICacheService {
+  //   user: IUser;
+     clearAllSession(): void;
+     clearAllStorage(): void;
+   }
   
   export class CacheService implements ICacheService {
     static ServiceId: string = 'CacheService'; 
     static $inject: Array<string> = ['$log', '$localStorage', '$sessionStorage'];
     
-    static USER: string = "user";
+    //static USER: string = "user";
     
     constructor(private $log: ng.ILogService, private $localStorage: any, private $sessionStorage: any) {    
     }
     
-    get user(): IUser {
-      return this.getSession(CacheService.USER);
-    }
-    set user(value: IUser) {
-      this.setSession(CacheService.USER, value);
-    }
+    // get user(): IUser {
+    //   return this.getSession(CacheService.USER);
+    // }
+    // set user(value: IUser) {
+    //   this.setSession(CacheService.USER, value);
+    // }
     
     clearAllSession() {
-      delete this.$sessionStorage[CacheService.USER];
+      //delete this.$sessionStorage[CacheService.USER];
     }
 
     clearAllStorage() {
