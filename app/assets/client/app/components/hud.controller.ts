ce67f3e442d22502a7f2f5d0d05921a73a3d6478
Paul Walker <github@paulwalker.tv>
2015-10-13 12:47:27 -0700

    wire up goal requirements in hud

--- app/assets/client/app/components/hud.controller.ts
+++ app/assets/client/app/components/hud.controller.ts
@@ -9,15 +9,10 @@ module powur {
     static ControllerId = 'HudController';
     static $inject = ['$scope', '$log', '$interval'];
     
-    fullName: string;
-    
     gridSize: number;
     dollarsEarned: number;
     co2: number;
     loginStreaks: number;
-    
-    personalProposals: number;
-    teamProposals: number;
 
     get home(): any {
       return this.$scope.home;
@@ -53,25 +48,12 @@ module powur {
                 private $log: ng.ILogService,
                 private $interval: ng.IIntervalService) {
       
-
-      $log.debug('reqs', this.goalRequirements);
-
-      // sample data
-      
       this.gridSize = 18;
       
       this.dollarsEarned = 1880.89;
       
       this.co2 = 72.36;
       this.loginStreaks = 26;
-
-      this.personalProposals = 10;
-      this.teamProposals = 0;
-      
-      this.$interval(() => {
-        this.personalProposals = this.personalProposals > 100 ? 20 : this.personalProposals + 1;
-        this.teamProposals = this.personalProposals > 100 ? 0 : this.personalProposals + 2;
-      }, 100, 0, true);
     }
 
   }
