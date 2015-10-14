84d1e2088be4a2d233fbe0a790c13f927f26e98b
Viphak Lay <viphak.lay@eyecuelab.com>
2015-10-13 21:23:46 -0700

    revert run

--- app/assets/client/app/client.run.ts
+++ app/assets/client/app/client.run.ts
@@ -11,33 +11,40 @@ module powur {
                 $state: ng.ui.IStateService,
                 $session: SessionService) {
 
-      $rootScope.$on('$stateChangeStart', (e: any,
-                                           toState: ng.ui.IState,
-                                           toParams: ng.ui.IStateParamsService,
-                                           fromState: ng.ui.IState,
-                                           fromParams: ng.ui.IStateParamsService) => {
-
-        if (/home\./.test(toState.name) && !$session.instance.loggedIn()) {
-          e.preventDefault();
-          $state.go('login.public');
-        }
-      });
-      
-      $rootScope.$on('$stateChangeSuccess', (e: any,
-                                             toState: ng.ui.IState,
-                                             toParams: ng.ui.IStateParamsService,
-                                             fromState: ng.ui.IState,
-                                             fromParams: ng.ui.IStateParamsService) => {
-      });
-
-      $rootScope.$on('$stateChangeError', (e: any,
-                                           toState: ng.ui.IState,
-                                           toParams: ng.ui.IStateParamsService,
-                                           fromState: ng.ui.IState,
-                                           fromParams: ng.ui.IStateParamsService,
-                                           error: any) => {
-      });
+    var stateChangeStart = (e: any, 
+                            toState: ng.ui.IState,
+                            toParams: ng.ui.IStateParamsService,
+                            fromState: ng.ui.IState,
+                            fromParams: ng.ui.IStateParamsService) => {
+      //$log.debug('stateChangeStart');
+      if (/home\./.test(toState.name) && !$session.instance.loggedIn()){
+        e.preventDefault();
+        $state.go('login.private');
+      }
     }
+    
+    var stateChangeSuccess = (e: any,
+                              toState: ng.ui.IState,
+                              toParams: ng.ui.IStateParamsService,
+                              fromState: ng.ui.IState,
+                              fromParams: ng.ui.IStateParamsService) => {
+      //$log.debug('stateChangeSuccess');
+    }
+    
+    var stateChangeError = (e: any,
+                            toState: ng.ui.IState,
+                            toParams: ng.ui.IStateParamsService,
+                            fromState: ng.ui.IState,
+                            fromParams: ng.ui.IStateParamsService,
+                            error: any) => {
+      //$log.debug('stateChangeError');
+    }
+  
+      $rootScope.$on('$stateChangeStart', stateChangeStart);      
+      $rootScope.$on('$stateChangeSuccess', stateChangeSuccess);
+      $rootScope.$on('$stateChangeError', stateChangeError);
+    }
+    
   }
   
   appModule.run(RunConfigs);
