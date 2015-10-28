e033a81453c70eb5a3e2ccc3b17e2a007129775f
Sasha Shamne <alexander.shamne@gmail.com>
2015-10-28 10:22:14 -0700

    Update paperclip to keep old s3 attachments. Closes #612

--- app/models/concerns/paperclip_scopes.rb
+++ app/models/concerns/paperclip_scopes.rb
@@ -5,7 +5,8 @@ module PaperclipScopes
     aws_bucket = ENV['AWS_BUCKET']
     aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
     aws_secret_key = ENV['AWS_SECRET_ACCESS_KEY']
-    has_attached_file :avatar,
+    has_attached_file(
+      :avatar,
       path:            '/avatars/:id/:basename_:style.:extension',
       url:             ':s3_domain_url',
       default_url:     'dashboard/default-avatar.png',
@@ -13,22 +14,25 @@ module PaperclipScopes
       s3_credentials:  { bucket:            aws_bucket,
                          access_key_id:     aws_access_key_id,
                          secret_access_key: aws_secret_key },
-    s3_protocol: :https,
-    styles:          {
-      thumb:   [ '128x128#', :jpg, quality: 70 ],
-      preview: [ '480x480#', :jpg, quality: 70 ],
-      large:   [ '600>',     :jpg, quality: 70 ],
-    retina:  [ '1200>',    :jpg, quality: 30 ] },
-    convert_options: {
-      thumb:   '-set colorspace sRGB -strip',
-      preview: '-set colorspace sRGB -strip',
-      large:   '-set colorspace sRGB -strip',
-    retina:  '-set colorspace sRGB -strip -sharpen 0x0.5' }
+      keep_old_files:  true,
+      s3_protocol:     :https,
+      styles:          {
+        thumb:   [ '128x128#', :jpg, quality: 70 ],
+        preview: [ '480x480#', :jpg, quality: 70 ],
+        large:   [ '600>',     :jpg, quality: 70 ],
+        retina:  [ '1200>',    :jpg, quality: 30 ] },
+      convert_options: {
+        thumb:   '-set colorspace sRGB -strip',
+        preview: '-set colorspace sRGB -strip',
+        large:   '-set colorspace sRGB -strip',
+        retina:  '-set colorspace sRGB -strip -sharpen 0x0.5' })
 
     # Validate content type
     # validates_attachment_content_type :avatar, content_type: /\Aimage/
     # Validate filename
-    validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/, /gif\Z/]
+    validates_attachment_file_name(
+      :avatar,
+      matches: [/png\Z/, /jpe?g\Z/, /gif\Z/])
     # Explicitly do not validate
     do_not_validate_attachment_file_type :avatar
   end
