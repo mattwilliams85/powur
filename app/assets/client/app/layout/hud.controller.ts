112b435ef7717f941160456eaed9717924ef200d
Sasha Shamne <alexander.shamne@gmail.com>
2016-01-15 14:20:38 -0800

    Update hud with temp leg numbers. Closes #957

--- app/assets/client/app/layout/hud.controller.ts
+++ app/assets/client/app/layout/hud.controller.ts
@@ -34,6 +34,18 @@ module powur {
       return this.home.requirements.entities;
     }
 
+    get goalPersonalRequirement(): ISirenModel {
+      return _.find(this.goalRequirements, function(i) {
+        return i.properties.team == false;
+      });
+    }
+
+    get goalTeamRequirement(): ISirenModel {
+      return _.find(this.goalRequirements, function(i) {
+        return i.properties.team == true;
+      });
+    }
+
     get isQualifiedPartnerCase(): boolean {
       return this.goalRequirements.length == 1 &&
                this.goalRequirements[0].properties.product_id == 1 &&
